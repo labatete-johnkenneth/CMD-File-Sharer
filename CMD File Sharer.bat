cd3ab96d5fe8639992def335a4a4ccdeddf96a6f
labatete-johnkenneth <151600683+labatete-johnkenneth@users.noreply.github.com>
2025-01-25 18:35:23 +0800

    Add files via upload

--- CMD File Sharer.bat
+++ CMD File Sharer.bat
@@ -1,5 +1,62 @@
 @echo off
 setlocal
+:mainloop
+set /p var="Enter a number: (Press [1] for file sharing/hosting. [2] for url sharing/hosting. [3] for url lengthening. [4] to delete a file.)"
+if %var%==1  (
+    goto filesharing
+) else if %var%==2 (
+    goto urlsharing
+) else if %var%==3 (
+    goto urllengthening
+) else if %var%==4 (
+    goto deletefile
+) else (
+    echo Invalid input, please try again.
+    goto mainloop
+)
+
+:filesharing
 for /f "delims=" %%i in ('powershell -command "Add-Type -AssemblyName System.Windows.Forms; $fileDialog = New-Object System.Windows.Forms.OpenFileDialog; $fileDialog.InitialDirectory = 'C:\'; $fileDialog.ShowDialog(); $fileDialog.FileName"') do set "file=%%i"
-curl -F "file=@%file%" https://0x0.st
+set /p var="Do you want a long url: ([1] means yes [2] means no)"
+if %var%==1  (
+    curl -i -F "file=@%file%" -F secret= https://0x0.st/
+    echo Save the X-Token somewhere so you can manually edit your file in the foreseeable future
+) else if %var%==2  (
+    curl -i -F "file=@%file%" https://0x0.st/
+    echo Save the X-Token somewhere so you can manually edit your file in the foreseeable future
+) else (
+    echo Invalid input, please try again.
+    goto filesharing
+)
+goto end
+
+:urlsharing
+set /p url="Enter the url: "
+:urlloop
+set /p var="Do you want a long url: ([1] means yes [2] means no)"
+if %var%==1  (
+    curl -i -F "url=%url%" -F secret= https://0x0.st/
+    echo Save the X-Token somewhere so you can manually edit your file in the foreseeable future
+) else if %var%==2  (
+    curl -i -F "url=%url%" https://0x0.st/
+    echo Save the X-Token somewhere so you can manually edit your file in the foreseeable future
+) else (
+    echo Invalid input, please try again.
+    goto urlloop
+)
+goto end
+
+:urllengthening
+set /p url="Enter the complete url: (e.g https://0x0.st/8HzA.mp4)"
+curl -i -F "url=%url%" -F secret= https://0x0.st/
+echo Save the X-Token somewhere so you can manually edit your file in the foreseeable future
+goto end
+
+:deletefile
+set /p token="Enter the token: "
+set /p url="Enter the url: (e.g https://0x0.st/8HzA.mp4)"
+curl -F token=%token% -F delete= %url%
+goto end
+
+:end
 pause
